6c3b84e4e4f314b4954a31ccafdda2eba9936ca4
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-24 16:49:48 -0400

    finished Mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -31,10 +31,10 @@ const resolvers = {
           },
           checkout: async (parent, args, context) => {
             const url = new URL(context.headers.referer).origin;
-            const order = new Order({ products: args.albums });
+            const order = new Order({ albums: args.albums });
             const line_items = [];
       
-            const { albums } = await order.populate('products').execPopulate();
+            const { albums } = await order.populate('albums').execPopulate();
       
             for (let i = 0; i < albums.length; i++) {
               const album = await stripe.albums.create({
@@ -67,8 +67,57 @@ const resolvers = {
           }
     },
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addOrder: async (parent, { albums }, context) => {
+            console.log(context);
+            if (context.user) {
+              const order = new Order({ albums });
+      
+              await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+      
+              return order;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+          updateUser: async (parent, args, context) => {
+            if (context.user) {
+              return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+          updateAlbum: async (parent, { _id, quantity }) => {
+            const decrement = Math.abs(quantity) * -1;
+      
+            return await Album.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          }
+
+
 
     }
-}
+};
 
 module.exports = resolvers;
