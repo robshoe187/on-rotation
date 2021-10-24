7e00555adfe2a9e608cfd7670da2a1a66476f9a9
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-24 13:15:45 -0400

    starting resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { User, Album, Order } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+
+    }
+}
+
+module.exports = resolvers;
