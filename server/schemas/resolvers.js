03888e2762313234d6d9fb8081f3698d5c05fc6f
Rob Shoemaker <rob.shoe187@gmail.com>
2021-10-24 13:49:08 -0400

    added resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,24 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
     Query: {
-
+        albums: async () => {
+            return await Album.find();
+        },
+        album: async (parent, { _id }) => {
+            return await Album.findById(_id)
+        },
+        user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id)
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          },
+        
     },
 
     Mutation: {
