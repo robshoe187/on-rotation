2094743b3ef26fdb9419042362249daabeed7f15
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-27 17:17:39 -0400

    adding in css to Order History and Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-import Header from "../components/Header"; 
+import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
-
 const Login = (props) => {
-
     const [formState, setFormState] = useState({ email: '', password: '' });
     const [login, { error }] = useMutation(LOGIN);
-
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -24,7 +21,6 @@ const Login = (props) => {
           console.log(e);
         }
       };
-    
       const handleChange = (event) => {
         const { name, value } = event.target;
         setFormState({
@@ -36,24 +32,24 @@ const Login = (props) => {
         <>
         <Header />
         <Nav />
-
-        <Link to="/signup">← Go to Signup</Link>
-
-        <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
+        <form className="loginForm" onSubmit={handleFormSubmit}>
+        <Link className="btn btn-light" to="/signup">← Go to Signup</Link>
+        <div className="col-md-6">
+          <label htmlFor="email" className="form-label">Email address:</label>
           <input
-            placeholder="youremail@test.com"
+            
+            className="form-control"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
           />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+        <div className="col-md-6">
+          <label htmlFor="pwd" className="form-label">Password:</label>
           <input
-            placeholder="******"
+            
+            className="form-control"
             name="password"
             type="password"
             id="pwd"
@@ -65,15 +61,12 @@ const Login = (props) => {
             <p className="error-text">The provided credentials are incorrect</p>
           </div>
         ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        <div className="flex-row flex-end" className="loginButton">
+          <button className="btn btn-primary" type="submit">Submit</button>
         </div>
       </form>
-
-
         <Footer />
         </>
-
     )
 }
 
